951d06b265bc8bdaaa099b29f32f878cded5da96
Gaston Ramos <ramos.gaston@gmail.com>
2010-03-12 22:28:24 -0300

    - First working version

---
+++ test/forrest_stories_test.rb
@@ -0,0 +1,19 @@
+require 'rubygems'
+require 'action_controller'
+require 'stories'
+require 'stories/runner'
+
+class ForrestStoriesTest < ActionController::IntegrationTest
+  story "As a stories user I want to see the stories report " +
+         "in a very nice format" do
+
+    def setup
+
+    end
+
+    scenario "A master account tries to forbid an account" do
+
+    end
+
+   end
+end
